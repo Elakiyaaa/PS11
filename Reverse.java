3ddeeef7b81d2bf8b144ba5020efc3b36898cd87
Elakiyaaa <elakthamizh@gmail.com>
2017-07-03 09:23:47 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+package PS11;
+
+import java.util.Scanner;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String a=s.next();
+String b=new StringBuffer(a).reverse().toString();
+System.out.println(b);
+	}
+
+	}
+
+
