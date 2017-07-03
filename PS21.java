08be0e9e43782ba8c838ad24fed735505dbdbdab
Elakiyaaa <elakthamizh@gmail.com>
2017-07-03 09:41:32 +0530

    Add files via upload

---
+++ PS21.java
@@ -0,0 +1,19 @@
+package PS11;
+
+import java.util.Scanner;
+
+public class PS21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String a=s.next();
+if(a.equals("SUNDAY")||a.equals("sunday")){
+	System.out.println("false");
+}
+else{
+	System.out.println("true");
+}
+	}
+
+}
